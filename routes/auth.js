c33eeedad2d7c7bcf6ec2d11578b89a0bf54227f
Enrique Liñian C <enrique.linian@gmail.com>
2021-05-09 19:24:03 -0500

    Fin sección 8

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+/*
+    path: api/login
+
+*/
+const { Router } = require('express') ;
+const { check } = require('express-validator');
+
+const { crearUsuario, login, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+
+const router = Router() ;
+
+router.post( '/new', [
+    check('nombre', 'El nombre es obligaorio').not().isEmpty(),
+    check('email', 'El email es obligaorio').isEmail(),
+    check('password', 'La contraseña es obligaorio').not().isEmpty(),
+    validarCampos
+], crearUsuario) ;
+
+
+router.post( '/', [
+    check('password', 'La contraseña es obligaorio').not().isEmpty(),
+    check('email', 'El email es obligaorio').isEmail(),
+], login ) ;
+
+// validarJWT,
+router.get( '/renew', validarJWT, renewToken ) ;
+
+
+module.exports = router ;
